abf08ef9ca0657bc0bb333724841a04f2792941e
Anmol <anmol32.an@gmail.com>
2021-04-09 08:15:14 +0530

    Updated

---
+++ flask/flask_dyanmic_url_binding/fbapp.py
@@ -0,0 +1,27 @@
+
+from flask import *
+app = Flask(__name__) 
+
+@app.route('/admin')
+def admin():
+    return 'admin'
+
+@app.route('/librarian')
+def librarion():
+    return 'librarian'
+
+@app.route('/student')
+def student():
+    return 'student'
+
+
+@app.route('/user/<name>')
+def user(name):
+    if name == 'admin':
+        return redirect(url_for('admin'))
+    if name == 'librarian':
+        return redirect(url_for('librarian'))
+    if name == 'student':
+        return redirect(url_for('student'))
+if	__name__=='__main__':
+    app.run(debug = True)
